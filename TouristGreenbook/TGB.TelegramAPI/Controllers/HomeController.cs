23048c232d2c1cf6020604a79c0a8e0dd70c823a
Oleksandr Zelinskyi <sashko.zel2000@gmail.com>
2019-03-26 00:29:12 +0200

    add project for telegram bot and remove unnecessary files

---
+++ TouristGreenbook/TGB.TelegramAPI/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TGB.TelegramAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from Tourist Greenbook!";
+        }
+    }
+}
