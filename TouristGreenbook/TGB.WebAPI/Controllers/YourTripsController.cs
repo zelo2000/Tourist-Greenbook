19f877046290fbd4e352f0694e5ee5dbde630d1c
RomanHorbachevskyi <gororogen@gmail.com>
2019-03-26 20:04:29 +0200

    Added Controllers/YourTripsController.cs to make possible work with user's Trips. Added Views/YourTrips/Index.cshtml to show Trips of the authorized user. Added Views/YourTrips/Trip.cshtml to show selected Trip from the list of Trips Modified Views/Shared/_Layout.cshtml to show Your Trips button.

---
+++ TouristGreenbook/TGB.WebAPI/Controllers/YourTripsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TGB.WebAPI.Models;
+
+namespace TGB.WebAPI.Controllers
+{
+    public class YourTripsController : Controller
+    {
+        private static Trip tempTrip;
+        private static List<Trip> tripList;
+        public IActionResult Index()
+        {
+            //var tripList = new List<Trip>();
+            tripList = new List<Trip>();
+            tripList.Add(new Trip{Id=1,Budget=100,City="Lviv"});
+            // TODO load Trips from DataBase
+            return View(tripList);
+        }
+
+        public IActionResult Trip(int? id)
+        {
+            //ViewData["id"] = id;
+
+            //tempTrip= = id;
+            // TODO load Trip by ID from database
+            //if (id.HasValue)
+            {
+                //if (id>0 && id<=tripList.Count)
+                {
+                    return View(tripList.ElementAt((int) id-1));
+                }
+            }
+            
+            //return string "Wrong trip Id";
+        }
+    }
+}
