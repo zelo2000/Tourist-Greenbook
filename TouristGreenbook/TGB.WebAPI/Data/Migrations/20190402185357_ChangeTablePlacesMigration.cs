fcb636795be39a01fa6e6c73cf3335a7b7170be3
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-04-02 21:58:23 +0300

    add migration to update Places table in db

---
+++ TouristGreenbook/TGB.WebAPI/Data/Migrations/20190402185357_ChangeTablePlacesMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TGB.WebAPI.Data.Migrations
+{
+    public partial class ChangeTablePlacesMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "WorkTimeStart",
+                table: "Places",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "WorkTimeFinish",
+                table: "Places",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "WorkTimeStart",
+                table: "Places",
+                nullable: false,
+                oldClrType: typeof(TimeSpan));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "WorkTimeFinish",
+                table: "Places",
+                nullable: false,
+                oldClrType: typeof(TimeSpan));
+        }
+    }
+}
