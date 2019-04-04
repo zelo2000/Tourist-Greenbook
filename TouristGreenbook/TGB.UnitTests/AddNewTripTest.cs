e1346399fa6299b825d0edc77df4ac2ea950bea5
Oleksandr Zelinskyi <sashko.zel2000@gmail.com>
2019-04-04 16:35:07 +0300

    Add test project

---
+++ TouristGreenbook/TGB.UnitTests/AddNewTripTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TGB.UnitTests
+{
+    [TestFixture]
+    public class AddNewTripTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
